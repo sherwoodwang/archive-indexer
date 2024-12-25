d641e9e7e6a4fd097ed431995d31c1a4be58893e
Sherwood Wang <sherwoodwang@users.noreply.github.com>
2024-12-26 00:39:03 +0800

    add "filter" subcommand

--- src/arindexer/cli.py
+++ src/arindexer/cli.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+from pathlib import Path
 
 from . import Archive, Processor
 
@@ -8,6 +9,7 @@ def archive_indexer():
     parser = argparse.ArgumentParser()
     parser.add_argument('--archive')
     parser.add_argument('subcommand')
+    parser.add_argument('arguments', nargs='*')
 
     args = parser.parse_args()
 
@@ -22,9 +24,21 @@ def archive_indexer():
         with Archive(processor, archive_path) as archive:
             if args.subcommand == 'rebuild':
                 archive.rebuild()
+            elif args.subcommand == 'filter':
+                _filter(archive, args.arguments)
             else:
                 print(f'Unknown subcommand: {args.subcommand}', file=sys.stderr)
                 sys.exit(1)
+
+
+def _filter(archive, arguments):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('output')
+    args = parser.parse_args(arguments)
+
+    archive.filter(Path(args.input), Path(args.output))
+
 
 if __name__ == '__main__':
     archive_indexer()
